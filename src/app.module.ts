dac3afc20d9d72ad1deef4fe3c004f7389b5bbbc
jéssica <jessy.ard11@gmail.com>
2024-05-18 15:31:11 -0300

    adding crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { PrismaService } from './database/prisma.service';
 import { RocketMembersRepository } from './repositories/rocket-members-repository';
 import { PrismaRocketMembersRepository } from './repositories/prisma/prisma-rocket-members-repository';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [
     PrismaService,
